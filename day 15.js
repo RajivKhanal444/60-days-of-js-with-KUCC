4cf5af8b813e93465dd8d1a682fc4da2153f8301
Rajiv Khanal <155733068+RajivKhanal444@users.noreply.github.com>
2024-06-17 21:52:55 +0545

    Add files via upload

---
+++ day 15.js
@@ -0,0 +1,14 @@
+// getelementbyid()
+const title = document.getElementById('heading');
+
+// getelement by classname()
+ const item = document.getElementsByClassName('Item-list')
+
+// getElement by tagname()
+const tag = document.getElementsByTagName('list')
+
+// query selector 
+const query= document.querySelector('div')
+
+// queryselect all
+const queryall= document.querySelectorAll('div')
